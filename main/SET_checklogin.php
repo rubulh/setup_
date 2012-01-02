a268b61edfbc7d6eb06c656555e3bc2c1b4fe293
root <root@arpanahaloi.(none)>
2012-01-02 18:56:45 +0530

    1.add a variable SET_THELOGINTABLE FOR THE TABLE FOR AUTHORISED  USERS in the file functionpage.php 2.note that the name and password need not be escaped;;it is escaped and hashed inside the function 3.the mysql connection needs to be present for the function to execute 4.this function if returns true will allow for login by the SET_newlogin function

---
+++ main/SET_checklogin.php
@@ -0,0 +1,34 @@
+<?php
+/*************************************************************************
+function SET_checklogin();
+checks if the login and pass so passed matches that Tin the table
+if yes unit logs the user in
+hashed password checked 
+mysql extension used so assumed that the connection is already there st the start of the script
+escaping and all other stuff is done in here
+*/
+
+function SET_checklogin($USERNAME,$PASS)
+{
+  $USERNAME=mysql_real_escape_string($USERNAME);
+  $HASHEDPASS=mysql_real_escape_string(md5(mysql_real_escape_string($PASS)));
+  $query_checklogin=mysql_query("SELECT * FROM $SET_THELOGINTABLE WHERE NAME='$USERNAME' AND PASSWORD='$HASHEDPASS'");
+  $answer_checklogin=mysql_fetch_array($query_checklogin);
+  $ansagain_checklogin=mysql_fetch_array($query_checklogin);
+  if(($ansagain_checklogin))
+    {
+      error_log("[[[[[[SET]>>>MULTIPLE ENTRIES FOUND FOR SAME USERNAME AND PASSWORD ($USERNAME,$HASHEDPASS)");
+    }
+  if(!$answer_checklogin)
+    {
+      return false;
+      exit(1);
+    }
+  else if($answer_checklogin)
+    {
+      return true;
+      exit(1);
+    }
+}
+
+?>
