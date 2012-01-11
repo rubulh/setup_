426a591db5a280c930458dd9148686453758d6c4
root <root@arpanahaloi.(none)>
2012-01-11 17:56:35 +0530

    the mysql connection file added in its basic form for testing

---
+++ main/SET_mysqlconnection.php
@@ -0,0 +1,9 @@
+<?php
+require_once("thesetupconfiguration.php");
+require_once("SET_baserandomstring.php");
+require_once("SET_randostring.php");
+require_once("SET_salt.php");
+$SET_BASIC_MYSQL_CONNECT=mysql_connect("$SET_THEMYSQLHOSTNAME","$SET_THEMYSQLUSERAME","$SET_THEMYSQLPASSWORD");
+$SET_BASIC_SELECT_DATABASE=mysql_select_db("$SET_THEMYSQLDBNAME");
+
+?>
