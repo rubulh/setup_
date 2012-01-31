cc5250f84cf1aebf24a06e7edc2bf117e08d1c6c
rubulhaloi <rubulhaloi2007@gmail.com>
2012-02-01 00:17:18 +0530

    the register script completed and the logged for a new user ll be set as 7 and all the fields other than the userid shall be set as 'xxxxxxx

---
+++ main/SET_register.php
@@ -0,0 +1,31 @@
+<?php
+	//the SET_register function 
+	//it does the following takes in username password checks it and validates that it and then inserts it into the database 
+	// and sets all the other fields in the field to the corressponding field as xxxxx appropiately
+
+
+function register($regusername,$regpass)
+{
+	require_once("SET_mysqlconnection.php");
+	
+	$theregusername=mysql_real_escape_string($regusername);
+	$theregpass=mysql_real_escape_string($regpass);
+	$querytoregister="INSERT INTO $SET_THEMYSQLLOGINTABLENAME (NAME,PASSWORD,LOGGED,LOGINTIMESTAMP,LASTTIMESTAMPAUTHKEY,AUTHKEY,BASE,SALT,COOKIEEXPIRY,SESSIONID,USERID,LOGOUTTIMESTAMP,TOTALLOGGEDTIME) VALUES('$theregusername','$theregpass','7','xxxxxxx','xxxxxxx','xxxxxxx','xxxxxxx','xxxxxxx','xxxxxxx','xxxxxxx','xxxxxxx','xxxxxxx','xxxxxxx','xxxxxxx')";
+	$ansregwuer=mysql_query($querytoregister);
+	$affred=mysql_affected_rows();
+	if($affre==(-1))
+		{
+		error_log("[[[[[[[SET]>>>the SET_login function could not update the database for the user login for USER,USERID ($USER,$extracted_user_id)");
+	      	whisk(2);
+	      	return false;
+	      	exit(1);
+		}
+	else if($affre==1)
+		{
+		return true;	
+		}
+
+}
+
+
+?>
