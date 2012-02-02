9f159bd50c140b1f43267765321b25c27719c600
rubulhaloi <rubulhaloi2007@gmail.com>
2012-02-02 23:51:53 +0530

    the regenerate function in its first form completed
    now got to add update function and then the thing is  over
    
    note that the update function runs only after the iflogged and regenerate have ensured that all the parameters for active login are in place

--- main/SET_regenerate.php
+++ main/SET_regenerate.php
@@ -10,6 +10,7 @@ the responsibilty of removing the session handle and related stuff ll be done by
 */
 function regenerate($USERID)
 {
+	require_once("SET_mysqlconnection.php");
 	//first check the details
 	$isthesessionset=false;
 	$isthecookieset=false;
@@ -22,6 +23,7 @@ function regenerate($USERID)
  	if($isthesessionset)
 	{
 	error_log("[[[[[[[SET]>>>the session ids are set stil the iflogged allowed the control to come upto the regenerate for the USERID($USERID)");
+	return false;
 	}
 	
 	else
@@ -36,8 +38,36 @@ function regenerate($USERID)
 		exit(1);
 		}
 	  else  {
-		//now set the cookies
-		
+		//now set the session
+		//that the session is already started one need not set the session only verify and update
+		$regenera_query=mysql_query("SELECT * FROM $SET_THEMYSQLLOGINTABLENAME WHERE 'USERID'='$thecurrentuserid'");
+      		$regenera_details=mysql_fetch_array($regenera_query);
+		$theauthkey=$regenera_details['AUTHKEY'];
+		$authkeyincookie=$_COOKIE['authkey'];
+		if(!(md5($theauthkey)==$theauthkeyincookie))
+			{
+			error_log("[[[[[[[SET]>>>the authkey in the cookie is not the has of that in the database;the control must not have been allowed upto this point by the iflogged function for USERID($USERID)");
+			return false;
+			exit(1);
+			}
+		else	
+			{
+			$_SESSION['authkey']=$theauthkeyincookie;
+			$thecursessid=session_id();
+			//update the table
+			$updatequery=mysql_query("UPDATE $SET_THEMYSQLLOGINTABLENAME SET SESSIONID='$thecursessid' where USERID='$USERID'");
+			if(!mysql_affected_rows())
+				{
+				error_log("[[[[[[[SET]>>>the regenerated userid could not be updated in the database in the regenerate file for USERID($USERID)");
+				return false;
+				exit(1);
+				}
+			else
+				{
+				return true;
+				exit(1);
+				}
+			}
 		}
 	}
 }
