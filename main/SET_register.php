918070bd885aa66ea0e3a69f0a23756af918917d
rubul <rubulhaloi2007@gmail.com>
2012-03-09 00:54:01 +0530

    corrected 1.minor typos corrected in the main table name 2.the SET_register function is working properly for startup registration and also detects attempt for duplication 3.the SET_login file parially corrected

--- main/SET_register.php
+++ main/SET_register.php
@@ -4,31 +4,34 @@
 	// and sets all the other fields in the field to the corressponding field as xxxxx appropiately
 
 
-function register($regusername,$regpass)
+function SET_register($regusername,$regpass)
 {
 	require_once("SET_mysqlconnection.php");
-	
 	$theregusername=mysql_real_escape_string($regusername);
 	$md5theregpass=md5(mysql_real_escape_string($regpass));
-	$querytoevaluate=mysql_query("SELECT * FROM $SET_THEMYSQLLOGINTABLENAME WHERE NAME='$theregusername'");
-	$ansevaluate=mysql_fetch_array($ansevaluate);
+	$querytoevaluate=mysql_query("SELECT * FROM $SET_THEMYSQLLOGINTABLE WHERE NAME='$theregusername'");
+	$ansevaluate=mysql_fetch_array($querytoevaluate);
 	if($ansevaluate)
 		{
 		return "ALREADYEXISTS";
 		exit(1);
 		}
 	
-	$querytoregister="INSERT INTO $SET_THEMYSQLLOGINTABLENAME (NAME,PASSWORD,LOGGED,LOGINTIMESTAMP,LASTTIMESTAMPAUTHKEY,AUTHKEY,BASE,SALT,COOKIEEXPIRY,SESSIONID,LOGOUTTIMESTAMP,TOTALLOGGEDTIME) VALUES('$theregusername','$md5theregpass','7','xxxxxxx','xxxxxxx','xxxxxxx','xxxxxxx','xxxxxxx','xxxxxxx','xxxxxxx','xxxxxxx','xxxxxxx','xxxxxxx')";
+	$querytoregister="INSERT INTO $SET_THEMYSQLLOGINTABLE (NAME,PASSWORD,LOGGED,LOGINTIMESTAMP,LASTTIMESTAMPAUTHKEY,AUTHKEY,BASE,SALT,COOKIEEXPIRY,SESSIONID,LOGOUTTIMESTAMP,TOTALLOGGEDTIME) VALUES('$theregusername','$md5theregpass','7','xxxxxxx','xxxxxxx','xxxxxxx','xxxxxxx','xxxxxxx','xxxxxxx','xxxxxxx','xxxxxxx','xxxxxxx')";
 	$ansregwuer=mysql_query($querytoregister);
-	$affred=mysql_affected_rows();
-	if($affre==(-1))
+	if(mysql_error())
 		{
-		error_log("[[[[[[[SET]>>>the SET_login function could not update the database for the user login for USER,USERID ($USER,$extracted_user_id)");
-	      	whisk(2);
+error_log("[[[[[[[SET]>>>".mysql_error());
+		}
+	$affred=mysql_affected_rows();
+	if($affred==(-1))
+		{
+		error_log("[[[[[[[SET]>>>the SET_register function could not update the database for the user registration for USER ($theregusername)");
+	      	SET_whisk(2);
 	      	return false;
 	      	exit(1);
 		}
-	else if($affre==1)
+	else if($affred==1)
 		{
 		return true;	
 		}
